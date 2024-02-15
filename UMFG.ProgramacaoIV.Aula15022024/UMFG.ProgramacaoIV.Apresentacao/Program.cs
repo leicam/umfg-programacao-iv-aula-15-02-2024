4ebadada320e850b80f766c3778810c0c3ac1546
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2024-02-15 20:36:28 -0300

    feat: tipos de referencia, abstração, herança, polimorfismo e encapsulamento com CSharp

---
+++ UMFG.ProgramacaoIV.Aula15022024/UMFG.ProgramacaoIV.Apresentacao/Program.cs
@@ -0,0 +1,29 @@
+﻿using UMFG.ProgramacaoIV.Dominio.Entidades;
+
+namespace UMFG.ProgramacaoIV.Apresentacao
+{
+    internal class Program
+    {
+        private const string C_USUARIO_PADRAO = "padrao@umfg.edu.br";
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Informe o nome do cliente: ");
+                var nome = Console.ReadLine();
+
+                Console.WriteLine("Informe o CPF do cliente: ");
+                var cpf = Console.ReadLine();
+
+                var cliente = new Cliente(nome, cpf, C_USUARIO_PADRAO, C_USUARIO_PADRAO);
+
+                Console.WriteLine(cliente.ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
